1571c3dd6b6a4fe970f220aeab577f2fbafdb0f2
mnichangxin <mnichangxin@163.com>
2017-02-25 23:52:40 +0800

    complete get username list

--- zhihu/get.py
+++ zhihu/get.py
@@ -16,7 +16,7 @@ class GetPage:
 	# 抓取页面内容
 	def getContent(self, url):
 		r = req.get(url)
-		content = html_parser.unescape(r.content)
+		content = html_parser.unescape(r.content.decode('unicode-escape').encode('utf-8'))
 
 		return content
 
@@ -28,27 +28,24 @@ class GetPage:
 
 		return number / 20 if number / 20 == 0 else number / 20 + 1 
 
-	def getPage(self, page):
-		content = self.getContent('https://www.zhihu.com/people/mnichangxin/following?page=' + page)
+	def getPage(self, offset):
+		content = self.getContent('https://www.zhihu.com/api/v4/members/mnichangxin/followees?include=data%5B*%5D.answer_count%2Carticles_count%2Cgender%2Cfollower_count%2Cis_followed%2Cis_following%2Cbadge%5B%3F(type%3Dbest_answerer)%5D.topics&offset=' + offset + '&limit=20')
 
-		# user_id.extend(re.findall('"url":"","urlToken":"(.*?)"', content))
-		# user_name.extend(re.findall('"gender":.*?,"name":"(.*?)"', content))
-		user_name.extend(re.findall('<span class="UserLink UserItem-name">.*?<a class="UserLink-link" target="_blank" href="(.*?)">(.*?)</a>.*?</span>', content))
+		user_id.extend(re.findall('"url_token": "(.*?)"', content))
+		user_name.extend(re.findall('"avatar_url_template": ".*?", "name": "(.*?)"', content))
 
 	def load(self):
 		number = self.getNumber()
 
 		for i in range(0, number):
-			self.getPage(str(i + 1))
+			self.getPage(str(i * 20)) # offset = (page - 1) * 20
 
 	def start(self):
 		self.load()
 		
 		f = open('data.txt', 'w')
 
-		# for i in range(0, len(user_name)):
-		# 	f.write('用户ID：' + user_name[i] + ', ' + '昵称：' + user_name[i] + '\n')
-		for i in range(0, len(user_name)):
-			f.write('昵称：' + str(user_name[i]) + '\n')
+		for i in range(0, len(user_id)):
+			f.write('用户ID：' + user_id[i] + ', ' + '昵称：' + user_name[i] + '\n')
 
 		f.close()
