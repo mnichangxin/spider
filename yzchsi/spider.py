ee280e6d3c36cdd9084794f2d4bc893bda644ada
mnichangxin <mnichangxin@163.com>
2021-07-20 19:20:19 +0800

    feat: kskm

--- yzchsi/spider.py
+++ yzchsi/spider.py
@@ -18,25 +18,40 @@ queryActionParams = {
 	'pageno': 0
 }
 
-def getPageNo():
-	url = 'https://yz.chsi.com.cn/zsml/queryAction.do'
-	res = req.post(url, queryActionParams)
-	soup = BeautifulSoup(res.content, 'html.parser')
-	hasLipBox = soup.find('div', class_='zsml-page-box').find('ul', class_='ch-page').find('li', class_='lip_input-box')
-	allPageDiv = soup.find('div', class_='zsml-page-box').find('ul', class_='ch-page').find_all('li', class_='lip')
-	endPageDiv = allPageDiv[-3] if hasLipBox else allPageDiv[-2]
-	pageNo = int(endPageDiv.find('a').get_text(separator='', strip=True))
-	return pageNo
+def saveToXls(allData):
+	if not os.path.exists('./xls'): os.makedirs('./xls')
+	f = xlwt.Workbook(encoding = 'utf-8')
+	table = f.add_sheet('sheet1', cell_overwrite_ok = True)
+	for i in range(len(allData)):
+		for j in range(len(allData[i])):
+			table.write(i, j, allData[i][j])
+	f.save('./xls/硕士专业目录-专业学位-社会工作.xls')
+	logging.info('爬取完成并存表...')
 
-def getSchData(schActionUrl):
-	res = req.get(schActionUrl)
+def getKskmData(kskmUrl):
+	res = req.get(kskmUrl)
+	soup = BeautifulSoup(res.content, 'html.parser')
+	trDiv = soup.select('tbody.zsml-res-items tr')
+	kskmData = []
+	for i in range(len(trDiv)):
+		tdDiv = trDiv[i].find_all('td')
+		for j in range(len(tdDiv)):
+			text = tdDiv[j].contents[0].replace(' ', '').replace('\r', '').replace('\n', '')
+			if i == 0: kskmData.append(text)
+			elif i > 0 and text != kskmData[j]: kskmData[j] = kskmData[j] + ' 或 ' + text
+	return kskmData
+
+def getSchData(schUrl):
+	res = req.get(schUrl)
 	soup = BeautifulSoup(res.content, 'html.parser')
 	trDiv = soup.find('table', class_='ch-table').find('tbody').find_all('tr')
 	schData = []
 	for i in range(len(trDiv)):
-			trData = []
 			tdDiv = trDiv[i].find_all('td')
-			for j in range(0, len(tdDiv)):
+			kskmUrl = 'https://yz.chsi.com.cn' + tdDiv[7].find('a').get('href')
+			kskmData = getKskmData(kskmUrl)
+			trData = []
+			for j in range(len(tdDiv)):
 				text = tdDiv[j].get_text(separator=' ', strip=True)
 				if text == '':
 					scriptDiv = tdDiv[j].find('script')
@@ -45,6 +60,7 @@ def getSchData(schActionUrl):
 						findText = pattern.findall(scriptDiv.contents[0])
 						if findText: text = findText[0]
 				trData.append(text)
+			trData.extend(kskmData)
 			schData.append(trData)
 	return schData
 
@@ -75,15 +91,15 @@ def getData(startNo, endNo):
 			allData.extend(newTrData)
 	return allData
 
-def saveToXls(allData):
-	if not os.path.exists('./xls'): os.makedirs('./xls')
-	f = xlwt.Workbook(encoding = 'utf-8')
-	table = f.add_sheet('sheet1', cell_overwrite_ok = True)
-	for i in range(len(allData)):
-		for j in range(len(allData[i])):
-			table.write(i, j, allData[i][j])
-	f.save('./xls/硕士专业目录-专业学位-社会工作.xls')
-	logging.info('爬取完成并存表...')
+def getPageNo():
+	url = 'https://yz.chsi.com.cn/zsml/queryAction.do'
+	res = req.post(url, queryActionParams)
+	soup = BeautifulSoup(res.content, 'html.parser')
+	hasLipBox = soup.find('div', class_='zsml-page-box').find('ul', class_='ch-page').find('li', class_='lip_input-box')
+	allPageDiv = soup.find('div', class_='zsml-page-box').find('ul', class_='ch-page').find_all('li', class_='lip')
+	endPageDiv = allPageDiv[-3] if hasLipBox else allPageDiv[-2]
+	pageNo = int(endPageDiv.find('a').get_text(separator='', strip=True))
+	return pageNo
 
 if __name__ == '__main__':
 	logging.info('开始爬取......')
