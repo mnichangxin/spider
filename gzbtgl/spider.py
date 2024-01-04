44367de48ce2c75aa33d69eb39137698dd4a245d
Changxin Li <mnichangxin@163.com>
2024-01-04 15:47:59 +0800

    feat: 更新爬虫逻辑

--- gzbtgl/spider.py
+++ gzbtgl/spider.py
@@ -5,15 +5,25 @@ import os, time, re, logging, xlwt
 import req
 from bs4 import BeautifulSoup
 
+reqPublicPath = 'http://nyt.hubei.gov.cn/gzbtgl/Bt21To23/pub'
+savePath = './xls/2023湖北农机轮式拖拉机类目.xls'
 queryParams = {
 	'__RequestVerificationToken': '',
-	'YearNum': 2020,
-	'n': '',
-	'IdCard': '',
-	'p': '拖拉机',
+	'YearNum': 2023,
+	'JiJuLeiXing': '轮式拖拉机',
+	'JiJuLeiXingCode': 200101,
+	'FactoryName': '',
+	'BusinessName': '',
+	'ChuCBH': '',
+	'StartGJRiQi': '',
+	'EndGJRiQi': '',
+	'StateValue': '',
+	'StateName': '',
 	'areaName': '',
 	'AreaCode': '',
-	'qy': ''
+	'qy': '',
+	'n': '',
+	't': ''
 }
 
 logging.basicConfig(level=logging.INFO)
@@ -22,46 +32,54 @@ logging.basicConfig(level=logging.INFO)
 def getReqVerToken(soup):
 	return soup.find('input', type='hidden')['value']
 
+# 获取 t
+def getReqT(soup):
+	findScript = soup.find('script', text=re.compile('var t ='))
+	tTextSearch = re.search(r'var t = \'(.*)\'', findScript.string)
+	t = tTextSearch.groups()[0]
+	return t
+
 # 更新 queryParams
-def updateQueryParams(reqVerToken):
+def updateQueryParams(reqVerToken, reqT):
 	queryParams['__RequestVerificationToken'] = reqVerToken
+	queryParams['t'] = reqT
 
 # 预爬取，获得表头和页码数
 def preGet():
 	# 获取初始的 __RequestVerificationToke
-	initialUrl = 'http://222.143.21.233:2018/pub/gongshi'
+	initialUrl = '{}/gongshi'.format(reqPublicPath)
 	initialRes = req.get(initialUrl)
 
 	initialSoup = BeautifulSoup(initialRes.content, 'html.parser')
 
 	hiddenInput = initialSoup.find('input', type='hidden')
 
-	updateQueryParams(getReqVerToken(initialSoup))
+	updateQueryParams(getReqVerToken(initialSoup), getReqT(initialSoup))
 
 	# 获取表头和页码数
-	url = 'http://222.143.21.233:2018/pub/GongShiSearch'
+	url = '{}/GongShiSearch'.format(reqPublicPath)
 	res = req.post(url, queryParams)
 
 	soup = BeautifulSoup(res.content, 'html.parser')
 
-	updateQueryParams(getReqVerToken(soup))
+	updateQueryParams(getReqVerToken(soup), getReqT(initialSoup))
 
 	tdList = soup.find('table').find('thead').find('tr').find_all('td')
 
 	thData = [tdList[i].find('span').string for i in range(len(tdList))]
 
-	pageCount = int(re.findall(r'\d+', soup.find('div', class_='divPage').find_all('a')[-1]['href'])[0])
+	pageCount = int(re.findall(r'pageIndex=(\d+)', soup.find('div', class_='divPage').find_all('a')[-1]['href'])[0])
 
 	return (thData, pageCount)
 
 # 爬取每一页的数据
 def getPage(pageNum):
-	url = 'http://222.143.21.233:2018/pub/GongShiSearch?pageIndex=' + str(pageNum)
+	url = '{}/GongShiSearch?pageIndex='.format(reqPublicPath) + str(pageNum)
 	res = req.post(url, queryParams)
 
 	soup = BeautifulSoup(res.content, 'html.parser')
 
-	updateQueryParams(getReqVerToken(soup))
+	updateQueryParams(getReqVerToken(soup), getReqT(soup))
 
 	trList = soup.find('table').find('tbody').find_all('tr')
 
@@ -89,8 +107,8 @@ def startGet(startPageNum, endPageNum, preData):
 
 	for i in range(startPageNum, endPageNum + 1):
 		if i % 15 == 0:
-			logging.info('限流，20s 之后继续')
-			time.sleep(20)
+			logging.info('限流，40s 之后继续')
+			time.sleep(40)
 
 		logging.info('开始爬取第 {} 页'.format(i))
 		data = getPage(i)
@@ -105,7 +123,7 @@ def startGet(startPageNum, endPageNum, preData):
 
 		if i == endPageNum:
 			if not os.path.exists('./xls'): os.makedirs('./xls')
-			f.save('./xls/2020河南农机拖拉机类目.xls')
+			f.save('{}'.format(savePath))
 			logging.info('爬取完成并存表！')
 
 		dataPool.extend(data)
@@ -115,6 +133,4 @@ def startGet(startPageNum, endPageNum, preData):
 if __name__ == '__main__':
 	preData = preGet()
 	pageCount = preData[1]
-
 	startGet(1, pageCount, preData)
-
